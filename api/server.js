296c8357dfa9ff0020fc013ac09d103490c28345
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-04-11 14:21:51 -0600

    Task 2 done thru write endpoints

--- api/server.js
+++ api/server.js
@@ -5,9 +5,60 @@ const UserModel = require('./users/model');
 const userServer = express();
 
 userServer.use(express.json());
-
+// test server get
 userServer.get('/', (req, res) => {
-    res.end('Server Works')
+    res.end('Server Works');
+});
+// Get All Users
+userServer.get('/api/users', (req, res) => {
+    UserModel.find()
+        .then(users => {
+            res.json(users);
+        });
+});
+//Get Specified User
+userServer.get('/api/users/:id', (req, res) => {
+    UserModel.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(user)
+            }
+        })
+})
+// Creates New User
+userServer.post('/api/users', (req, res) => {
+    let user = req.body;
+    UserModel.insert(user)
+        .then(user => {
+            res.status(201).json(user);
+        });
+});
+// Delete Users 
+userServer.delete('/api/users/:id', (req, res) => {
+    UserModel.remove(req.params.id)
+    .then(user => {
+        if(!user){
+         res.status(404).json({ message: "The user with the specified ID does not exist" }); 
+        } else {
+            res.json(user);
+        }
+    });
+});
+// Update Users
+userServer.put('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    let changes = req.body;
+
+    UserModel.update(id, changes)
+        .then(updatedUser => {
+            if(!updatedUser){
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(updatedUser)
+            }
+        })
 })
 
 module.exports = userServer; // EXPORT YOUR SERVER instead of {}
